6038e5d933b2ccd3a686f06ee9afcaed2073c1b7
alalfakawma <alalfakawma@live.com>
2020-11-03 15:06:01 +0530

    Removed everything, redoing the server

--- handler.ts
+++
@@ -1,8 +0,0 @@
-import awsServerlessExpress from 'aws-serverless-express';
-import app from './src/index';
-
-const server = awsServerlessExpress.createServer(app);
-
-exports.handler = (event: any, context: any) => {
-    return awsServerlessExpress.proxy(server, event, context);
-};
